0c32c6d25dc38f76f78622e2b225a36baf503e57
LaurentiuStoleriu <lstoler@staff.uaic.ro>
2022-07-24 12:28:01 +0300

    ignore done right

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/lali/TITAN-ROG-sync/c/TiOX-Ritwika
-# Build directory: /home/lali/TITAN-ROG-sync/c/TiOX-Ritwika/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
